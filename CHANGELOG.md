0142c3471ab7ea0c6d208dc53833a09e13186434
ollie <davisbruce413@gmail.com>
2023-11-25 14:29:31 -0600

    Add Dev Route Picker
    Signed-off-by: ollie <davisbruce413@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,16 +2,16 @@
 
 ## *The Beta Edition*
 
-- Created in the dark, dark past...
+- [v0.0.1] Created in the dark, dark past...
 
 ## v1.0.0 *The Basic Edition*
 
-- Created November 24 2023
+- [v1.0.0] Created November 24 2023
+- [v1.1.0] Added Title Card (#3)
+- [v1.1.1] Minor Problem Fixes
+- [v1.1.2] Add Dev Route Picker
 
 ## v2.0.0 *The Usable Edition*
-
-- Added Title Card (#3)
-- Minor Problem Fixes
 
 ## v3.0.0 *The Advanced Edition*
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
@@ -21,10 +22,26 @@ public class Main {
 
         Logger.log(SectionName.PROGRAM, "Begin");
 
-
-        Assessor assessor = new Assessor();
-        assessor.run();
+        if (askDev()) {
+            // Do dev stuff
+        } else {
+            Assessor assessor = new Assessor();
+            assessor.run();
+        }
 
         Logger.log(SectionName.PROGRAM, "Finish");
+    }
+
+    private static boolean askDev() {
+        Logger.log(SectionName.CONSTRUCTOR, "Use dev mode? (Y/N): ", false);
+        String devResponse = new Scanner(System.in).nextLine();
+        if (devResponse.equals("Y")) {
+            return true;
+        } else if (devResponse.equals("N")) {
+            return false;
+        } else {
+            Logger.log(SectionName.PROGRAM, "Invalid response. Defaulting to no");
+            return false;
+        }
     }
 }
