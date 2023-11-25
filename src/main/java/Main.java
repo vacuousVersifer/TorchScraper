52461dd6452a1831585f056604fa2f00db46333d
ollie <davisbruce413@gmail.com>
2023-11-25 13:37:29 -0600

    Added Title Card
    Signed-off-by: ollie <davisbruce413@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,22 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
+        String message =
+                """
+                        ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+                        ║                                                                                                                        ║
+                        ║ mmmmmmm #                   mmmmmmm                      #             mmmm                                            ║
+                        ║    #    # mm    mmm            #     mmm    m mm   mmm   # mm         #"   "  mmm    m mm   mmm   mmmm    mmm    m mm  ║
+                        ║    #    #"  #  #"  #           #    #" "#   #"  " #"  "  #"  #        "#mmm  #"  "   #"  " "   #  #" "#  #"  #   #"  " ║
+                        ║    #    #   #  #""\""           #    #   #   #     #      #   #            "# #       #     m""\"#  #   #  #""\""   #     ║
+                        ║    #    #   #  "#mm"           #    "#m#"   #     "#mm"  #   #        "mmm#" "#mm"   #     "mm"#  ##m#"  "#mm"   #     ║
+                        ║                                                                                                                        ║
+                        ╚════════════════════════════════════════════════╗      Created By      ╔════════════════════════════════════════════════╝
+                                                                         ║ The VacuousVersifier ║                                                \s
+                                                                         ╚══════════════════════╝                                                \s
+                        """;
+        Logger.log(SectionName.SILENT, message);
+
         Logger.log(SectionName.PROGRAM, "Begin");
 
 
